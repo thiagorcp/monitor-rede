1eecaf1b7280274f84256b66e80e2eaaa6fbb75d
thiago.rogerio.pereira@gmail.com <thiago.rogerio.pereira@gmail.com>
2024-10-24 17:04:31 -0300

    modificação no script

--- network.py
+++ network.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 
 def envia_pings(host):
@@ -7,34 +6,22 @@ def envia_pings(host):
 
     -n: Indica a quantidade de pacotes a serem enviados.
     -w: Indica o tempo de espera para resposta em milisegundos.
-    nt: Indica que o sistema operacional é o Windows.
     
     """
-    sistema_operacional = os.name
 
-    if sistema_operacional == "nt":
-        comando_ping = f"ping -n 1 -w 2 {host}"
-    else:
-        comando_ping = f"ping -c 1 {host}"
-    
-    #resposta = os.system(comando_ping)
+    comando_ping = "ping -n 1 -w 2 " + host
 
-    # Limpa a saída padão do comando Ping, para não exibir na tela.
     resposta_do_host = subprocess.run(comando_ping, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     
 
     if resposta_do_host.returncode == 0:
-        print(f"{host} está ativo.")
+        print(f"{host}: Está ativo!")
     else:
-        print(f"{host} não respondeu.")
+        print(f"{host}: Não respondeu!")
     
 
 
 if __name__ == "__main__":
     # Base IP, exemplo.
-    base_ip = "192.168.100."
-    
-    # Varre os IPs de 1 a 10.
-    for i in range(1, 10):
-        ip = base_ip + str(i)
-        envia_pings(ip)
+    hosts = ("10.0.40.169", "127.0.0.1", "10.0.40.170")
+    envia_pings(hosts[2])
