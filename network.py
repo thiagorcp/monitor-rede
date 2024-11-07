fb8fff16fd7d5ca862c823a768faa008ec48e2ae
thiago.rogerio.pereira@gmail.com <thiago.rogerio.pereira@gmail.com>
2024-11-07 17:55:03 -0300

    refatoração do script

--- network.py
+++ network.py
@@ -1,20 +1,2 @@
-import subprocess
-import ips
-
-ips = ips.host_rede()
-link = "https://www.youtube.com/watch?v=R26iojTwUv8"
-def envia_pings():
-
-    while True:
-        for chave, valor in ips.items():
-            comando_ping = "ping -n 1 -w 3 " + valor
-            resposta_do_host = subprocess.run(comando_ping, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            if not resposta_do_host.returncode:
-                print(chave, ": ", valor, " tá vivo!")
-            else:
-                print(chave, ": ", valor, " tá morto!")
-        break
-
-    return None
-
-envia_pings()
+def main():
+    pass
--- status-hosts/get-status.py
+++ status-hosts/get-status.py
@@ -1,9 +1,34 @@
-import network
+import ips
+import subprocess
+import platform
 from fastapi import FastAPI
 
 api_test = FastAPI()
-hosts_tests = network.status_hosts()
+hosts = ips.hosts_rede()
+
+def envia_pings(host):
+    status_host = False
+    for name_host, value_ip in hosts.items():
+        send_ping = ["ping", "-c", "1", value_ip] if platform.system() == "Windows" else ["ping", "-n", "1", value_ip]
+
+        resp_status_ping = subprocess.run(send_ping, capture_output=True, text=True, timeout=2)
+        if resp_status_ping.returncode:
+            status_host = True
+            print(f"{name_host}: {status_host}")
+        else:
+            #print(f"{name_host}: {status_host}")
+            print(f"{name_host} não respondeu: (Tempo limite excedido!).")
+
+    return None
+
+
 
 @api_test.get("/status-host")
 def get_status_hosts():
-    pass
+    status_ping = envia_pings()
+    return status_ping
+
+
+if __name__ == "__main__":
+
+    print(envia_pings(hosts))
