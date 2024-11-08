ba230d5816f218c3b3627359333e66276d2cc70c
Thiago Pereira <thiago.rogerio.pereira@gmail.com>
2024-11-08 02:58:56 -0300

    refatoracao no script

--- info_rede/get_status_rede.py
+++ info_rede/get_status_rede.py
@@ -1,24 +1,19 @@
 import subprocess
 from ips import hosts_rede
 
+
 def retorna_status_ping():
     hosts = hosts_rede()
-    status_host = False
     lists_status_hosts = dict()
 
     for name_host, value_ip in hosts.items():
-        send_ping = ["ping", "-n", "1", value_ip]
-
+        cmd = ["ping", "-n", "1", value_ip]
         try:
-            resp_status_ping = subprocess.run(send_ping, capture_output=True, text=True, timeout=3)
+            resp_status_ping = subprocess.run(cmd, capture_output=True, text=True, timeout=2)
             if resp_status_ping.returncode == 0:
-                status_host = True
-                lists_status_hosts[name_host] = status_host
+                lists_status_hosts[name_host] = True
         except subprocess.TimeoutExpired:
-            lists_status_hosts[name_host] = status_host
+            lists_status_hosts[name_host] = False
+            pass
 
     return lists_status_hosts
-
-
-if __name__ == "__main__":
-    pass
