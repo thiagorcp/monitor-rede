02af4b3c27f96f2ac852f445b86558de215d509d
Thiago Pereira <thiago.rogerio.pereira@gmail.com>
2024-11-09 17:17:30 -0300

    refatoração

--- backend/monitoramento_rede/info_rede.py
+++ backend/monitoramento_rede/info_rede.py
@@ -1,19 +1,45 @@
+import json
+import platform
 import subprocess
-from ips import hosts_rede
+
+def json_for_dict():
+    with open('ips_test.json', 'r') as json_file:
+        ips_test = json.load(json_file)
+    return ips_test
+
+def envia_ping():
+    ips_test = json_for_dict()
+
+    if platform.system().lower() == 'windows':
+        cmd = "ping " + "-n" + " 1 "
+        for k, v in ips_test.items():
+            print(k, v)
+        #output_subprocess = subprocess.run(args=(cmd + ip), capture_output=True, text=True, timeout=2)
+
+        return ""
 
 
-def retorna_status_ping():
-    hosts = hosts_rede()
-    lists_status_hosts = dict()
 
-    for name_host, value_ip in hosts.items():
-        cmd = ["ping", "-n", "1", value_ip]
-        try:
-            resp_status_ping = subprocess.run(cmd, capture_output=True, text=True, timeout=2)
-            if resp_status_ping.returncode == 0:
-                lists_status_hosts[name_host] = True
-        except subprocess.TimeoutExpired:
-            lists_status_hosts[name_host] = False
-            pass
 
-    return lists_status_hosts
+
+
+
+
+
+#def retorna_status_ping():
+#    hosts = hosts_rede()
+#    lists_status_hosts = dict()
+
+    #for name_host, value_ip in hosts.items():
+    #    cmd = ["ping", "-n", "1", value_ip]
+    #    try:
+    #        resp_status_ping = subprocess.run(cmd, capture_output=True, text=True, timeout=2)
+    #        if resp_status_ping.returncode == 0:
+    #            lists_status_hosts[name_host] = True
+    #    except subprocess.TimeoutExpired:
+    #        lists_status_hosts[name_host] = False
+    #        pass
+
+    #return lists_status_hosts
+
+envia_ping()
